9e0c4460e2bc8097a71f1a0ea2fcc2c88163c01c
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-26 12:43:34 -0700

    added more code to Author.php file

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -4,10 +4,11 @@ namespace Nwoodard\ObjectOriented;
 require_once("autoload.php");
 require_once(dirname(_DIR_, 2) . "/vendor/autoload.php");
 
+use http\Exception\InvalidArgumentException;
 use Ramsey\Uuid\Uuid;
 
 /**
- * Author profile
+ * author profile
  *
  *This profile is for learning purposes.
  *
@@ -18,6 +19,93 @@ class Author implements \JsonSerializable {
 			use ValidateDate;
 			use ValidateUuid;
 			/**
-			 *
-			 */
+			 * id for Author; this is the primary key
+			 * @var Uuid $authorId
+			 **/
+			private $authorId;
+			/**
+			 * id of the author; this is a foreign key
+			 * @var Uuid $authorId;
+			 **/
+			 private $authorAvatarUrl;
+			 /**
+			  * actual textual content of this author
+			  * @var string $authorContent
+			  **/
+			 private $authorActivationToken;
+			 /**
+			  * date and time this Author was sent, in a PHP DateTime object
+			  * @var \DateTime $authorDate
+			  **/
+			 private $authorEmail;
+			 /**
+			  * e-mail address of Author
+			  * @var
+			  */
+
+			 private $authorHash;
+			 /**
+			  * encripted password
+	 */
+			 private $authorUsername;
+			 /**
+			  * constructor for this Author
+			  *
+			  * @param string|Uuid $newAuthorId id of this Author or null if a new Author
+			  * @param string|Uuid $newAuthor
+			  * @param string $newAuthorActivationToken string containing actual author data
+			  * @param \DateTime|string|null $newAuthorDate date and time author was activated or null if set to current date and time
+			  * @throws \InvalidArgumentException if data types are not InvalidArgumentException
+			  * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+			  * @throws \TypeError if data types violate type hints
+			  * @throws \Exception if some other exception occurs
+			  * @Documentation https://php.net/manual/en/languages.oop5.decon.php
+			  **/
+			 public function _construct($newAuthorId, $newAuthorAvatarURL,  $newAuthorActivationToken, $newAuthorEmail, string $newAuthorHash, $newAuthorUsername){
+			 	try {
+			 		$this->setAuthorId($newAuthorId);
+			 		$this->setAuthorAvatarUrl($newAuthorAvatarURL);
+			 		$this->setAuthorActivationToken($newAuthorActivationToken);
+			 		$this->setAuthorEmail($newAuthorEmail);
+			 		$this->setAuthorHash($newAuthorHash);
+			 		$this->setAuthorUsername($newAuthorUsername);
+			 	}
+			 }
+			 /**
+			  * accessor method for author id
+			  *
+			  * @return Uuid value of author id
+			  **/
+public function getAuthorId() : Uuid {
+			return($this->authorId);
+}
+
+/**
+ * mutator method for author id
+ *
+ * @param Uuid|string $newAuthorId is not positive
+ * @throws \RangeException if $newAuthorId is not positive
+ * @throws \TypeError if $newAuthorId is not a uuid or string
+ **/
+public function setAuthorId($newAuthorId) : void {
+			try {
+				$uuid = self ::validateUuid($newAuthorId);
+			} catch(InvalidArguementException | \RangeException |Exception |\TypeError $exception){
+				$exceptionType = get_class($exception);
+				throw(new $exceptionType ($exception->getMessage(), 0, $exception));
+			}
+			//convert and store author id
+			$this ->authorAvatarUrl = $uuid;
+}
+/**
+ * accessor method for author avatar url
+ *
+ * @return string value of author avatar url
+ **/
+public function getauthorAvatarUrl() : Uuid{
+        return($this->authorAvatarUrl);
+/**
+ * mutator method for author avatar url
+ *
+ * @param string $new
 }
