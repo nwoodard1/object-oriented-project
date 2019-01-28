88fff0d3fb3ab624238444185b50f01dc2737a2b
Natalie Woodard <nwoodard1@cnm.edu>
2019-01-28 14:45:28 -0700

    Fixed a lot of errors inside of my Author.php file

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -2,10 +2,10 @@
 namespace Nwoodard\ObjectOriented;
 
 require_once("autoload.php");
-require_once(dirname(_DIR_, 2) . "/vendor/autoload.php");
+require_once(dirname(_DIR_, 2) . "/Classes/Author.php");
 
-use http\Exception\BadQueryStringException;
-use http\Exception\InvalidArgumentException;
+//use http\Exception\BadQueryStringException;
+//use http\Exception\InvalidArgumentException;
 use Ramsey\Uuid\Uuid;
 
 /**
@@ -16,54 +16,63 @@ use Ramsey\Uuid\Uuid;
  * @Author Natalie Woodard <nwoodard1@cnm.edu>
  * @version 3.0.0
  **/
-class Author implements \JsonSerializable {
-	use ValidateDate;
+class Author {
 	use ValidateUuid;
+
 	/**
 	 * id for Author; this is the primary key
 	 * @var Uuid $authorId
 	 **/
 	private $authorId;
+
 	/**
-	 * id of the Author; this is a foreign key
-	 * @var Uuid $authorId ;
+	 * avatar of the Author; this is a foreign key
+	 * @var Uuid $authorAvatarUrl ;
 	 **/
 	private $authorAvatarUrl;
+
 	/**
-	 * avatar image of this author
-	 * @var string $authorAvatarUrl
+	 * activation token for Author
+	 * @var string $authorActivationToken
 	 **/
 	private $authorActivationToken;
+
 	/**
-	 * date and time this Author signed in, in a PHP DateTime object
-	 * @var \DateTime $authorActivationToken
+	 * email for Author
+	 * @var string $authorEmail
 	 **/
 	private $authorEmail;
-	/**
-	 * e-mail address of Author
-	 * @var
-	 */
 
-	private $authorHash;
 	/**
-	 * encrypted password assigned to author
+	 * encrypted password of Author
+	 * @var string $authorHash
+	 */
+	private $authorHash;
+
+	/**
+	 * username of Author
+	 * @var string $authorUsername
 	 */
 	private $authorUsername;
+
 
 	/**
 	 * constructor for this Author
 	 *
 	 * @param string|Uuid $newAuthorId id of this Author or null if a new Author
-	 * @param string|Uuid $newAuthor
-	 * @param string $newAuthorActivationToken string containing actual author data
-	 * @param \DateTime|string|null $newAuthorDate date and time author was activated or null if set to current date and time
+	 * @param string $newAuthorAvatarUrl string containing author avatar
+	 * @param string $newAuthorActivationToken string containing activation token
+	 * @param string|null $newAuthorEmail string author was activated or null
+	 * @param string $newAuthorHash string containing encrypted password for author
+	 * @param string $newAuthorUsername string containing the username of the author
 	 * @throws \InvalidArgumentException if data types are not InvalidArgumentException
 	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
 	 * @throws \TypeError if data types violate type hints
 	 * @throws \Exception if some other exception occurs
 	 * @Documentation https://php.net/manual/en/languages.oop5.decon.php
 	 **/
-	public function _construct($newAuthorId, $newAuthorAvatarURL, $newAuthorActivationToken, $newAuthorEmail, string $newAuthorHash, $newAuthorUsername) {
+
+	public function __construct($newAuthorId, $newAuthorAvatarURL, $newAuthorActivationToken, $newAuthorEmail, string $newAuthorHash, $newAuthorUsername) {
 		try {
 			$this->setAuthorId($newAuthorId);
 			$this->setAuthorAvatarUrl($newAuthorAvatarURL);
@@ -71,15 +80,19 @@ class Author implements \JsonSerializable {
 			$this->setAuthorEmail($newAuthorEmail);
 			$this->setAuthorHash($newAuthorHash);
 			$this->setAuthorUsername($newAuthorUsername);
-		}
-			 }
+			//determine what exception type was thrown
+		} catch(\InvalidArgumentException | \RangeException | \TypeError $exception) {
+				$exceptionType = get_class($exception);
+				throw(new $exceptionType ($exception->getMessage(),0, $exception));
+			}
+	}
 
 	/**
 	 * accessor method for author id
 	 *
-	 * @return Uuid value of author id
+	 * @return string value of author id (null if it is a new Author)
 	 **/
-	public function getAuthorId(): Uuid {
+	public function getAuthorId(): string {
 		return ($this->authorId);
 	}
 
@@ -87,6 +100,7 @@ class Author implements \JsonSerializable {
 	 * mutator method for author id
 	 *
 	 * @param Uuid|string $newAuthorId is not positive
+	 * @throws \InvalidArgumentException if the id is not a string or is insecure
 	 * @throws \RangeException if $newAuthorId is not positive
 	 * @throws \TypeError if $newAuthorId is not a uuid or string
 	 **/
@@ -97,8 +111,9 @@ class Author implements \JsonSerializable {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType ($exception->getMessage(), 0, $exception));
 		}
+
 		//convert and store author id
-		$this->authorAvatarUrl = $uuid;
+		$this->authorId = $uuid;
 	}
 
 	/**
@@ -106,6 +121,37 @@ class Author implements \JsonSerializable {
 	 *
 	 * @return string value of author avatar url
 	 **/
+
+	public function getAuthorAvatarUrl(): string {
+		return ($this->authorAvatarUrl);
+	}
+
+	/**
+	 * mutator method for author avatar url
+	 *
+	 * @param string $newAuthorAvatarUrl new value author avatar url
+	 * @throws \InvalidArgumentException if the author url is empty
+	 * @throws \RangeException if the url is longer than 255 characters
+	 **/
+
+	public function setAuthorAvatarUrl(string $newAuthorAvatarUrl) {
+		if(empty($newAuthorAvatarUrl) == true){
+			throw(new \InvalidArgumentException("This URL is empty."));
+		}
+		//verify the URL is no longer than 255 characters
+		if(strlen($newAuthorAvatarUrl)>255) {
+			throw(new \RangeException("This URL is too long. It must be no longer than 255 characters."));
+		}
+		//Store the author avatar URL
+		$this->authorAvatarUrl = $newAuthorAvatarUrl;
+	}
+
+	/**
+	 * accessor method for author activation token
+	 *
+	 * @return string value of author activation token
+	 **/
+
 	public function getAuthorActivationToken(): string {
 		return ($this->authorActivationToken);
 	}
@@ -115,32 +161,29 @@ class Author implements \JsonSerializable {
 	 *
 	 * @param string $newAuthorActivationToken
 	 * @throws \InvalidArgumentException if $newAuthorActivationToken is not a string or insecure
-	 * @throws \RangeExeption if $newAuthorActivationToken is > 140 characters
+	 * @throws \RangeException if $newAuthorActivationToken is > 140 characters
 	 * @throws \TypeError if $newAuthorActivationToken is not a string
 	 **/
 	public function setAuthorActivationToken(string $newAuthorActivationToken): void {
 		//verify the author activation token is secure
 		// 	$newAuthorActivationToken = trim ($newAuthorActivationToken};
-		$newAuthorActivationToken = filter_var($newAuthorActivationToken, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-		if(empty($newAuthorActivationToken) === true) {
-			throw(new\InvalidArgumentException("author activation token is empty or insecure"));
+		//$newAuthorActivationToken = filter_var($newAuthorActivationToken, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(strlen($newAuthorActivationToken) !== 32) {
+			throw(new \RangeException("author activation token must be 32 characters long"));
 		}
-
-		// verify the author activation token will fit in the database
-		if(strlen($newAuthorActivationToken) > 140) {
-			throw(new \RangeException("author activation token is too large"));
-
-		}
-		//store the author activation token
+		//Store the author activation token
 		$this->authorActivationToken = $newAuthorActivationToken;
 	}
 
 	/**
-	 * accessor method for AuthorEmail
+	 * accessor method for author email
 	 *
-	 * @return \DateTime value of author email
+	 * @param string $newAuthorEmail new value author email
+	 * @throws \InvalidArgumentException if the author activation token isn't a string or is insecure
+	 * @throws \RangeException if the token is not exactly 32 characters
 	 **/
-	public function getAuthorEmail(): \DateTime {
+
+	public function getAuthorEmail(): string {
 		return ($this->authorEmail);
 	}
 
@@ -262,5 +305,15 @@ public static function getAuthorByAuthorAvatarUrl(\PDO $pso, $authorAvatarUrl) :
 	/		$parameters = ["authorAvatarUrl" => $authorAvatarUrl->getBytes()];
 			$statement->execute($parameters);
 			//build an array of authors
+			$author = new \SplFixedArray($statement->rowCount());
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			while(($row = $statement->fetch()) !==false){
+						try {
+							$author = new Author($row["authorId"], $row["authorAvatarUrl"], $row["authorActivationToken"], $row["authorEmail"], $row["authorHash"], $row["authorUsername"]);
+							$authors[$authors->key()] = $author;
+							$authors->next();
+
+
+			}
 }
 }
